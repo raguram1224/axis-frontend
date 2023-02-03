52d683eff11cc6b13ed58ee0e3c5d68d7ae77c93
raguram1224 <114975955+raguram1224@users.noreply.github.com>
2023-02-03 13:58:05 +0530

    Update and rename OurServices.jsx to OurServices.js

--- src/pages/OurServices.js
+++ src/pages/OurServices.js
@@ -119,14 +119,14 @@ const OurServices = () => {
       
     </section>
 
-    <script
+   
       type="module"
-      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
-    ></script>
-    <script
+      src={"https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"}
+    
+    {/* <script
       nomodule
       src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
-    ></script>
+    ></script> */}
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
